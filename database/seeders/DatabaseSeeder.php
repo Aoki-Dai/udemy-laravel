3e4911fffca88dbcf705a69789c8171f026c431b
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-07-14 06:18:12 +0900

    ダミーデータ(シーダー)の追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            TestSeeder::class,
+            UserSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
