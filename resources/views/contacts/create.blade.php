3fb23127a36a1a3d37ac9bca44a63dd0f1fb953c
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-07-14 05:16:07 +0900

    バリデーションの追加

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -9,6 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <x-input-error class="mb-4" :messages="$errors->all()"/>
                     <section class="relative text-gray-600 body-font">
                         <form method="post" action="{{ route('contacts.store') }}">
                             @csrf
