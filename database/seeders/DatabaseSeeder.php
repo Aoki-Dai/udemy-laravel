6f3affdd4294be970ea06b0f313cd8ef73eb7cf3
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-07-18 21:36:24 +0900

    ダミーデータ(Factory & Faker)[大量のダミーデータ]

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
             TestSeeder::class,
             UserSeeder::class,
         ]);
-        // \App\Models\User::factory(10)->create();
+
+        \App\Models\ContactForm::factory(100)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
