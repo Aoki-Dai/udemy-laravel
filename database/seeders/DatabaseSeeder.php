e6611015c89600a96cb8b26325401ff7cf83ae93
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2024-07-19 00:27:01 +0900

    [ リレーション(1対多) ] 機能追加: AreaおよびShopモデル、マイグレーション、およびシーダーの追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TestSeeder::class,
             UserSeeder::class,
+            AreaSeeder::class,
+            ShopSeeder::class,
         ]);
 
         \App\Models\ContactForm::factory(100)->create();
